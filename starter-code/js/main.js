8b47e49b44d73915c8f88090bb1b7a2cc2939c9e
kaya70875 <shortsforturkey@gmail.com>
2024-09-24 15:56:50 +0300

    add destination page

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,8 +1,20 @@
 const navItems = document.querySelectorAll('.list-item');
 
+const previousItem = localStorage.getItem('activeItem');
+console.log(previousItem);
+if (previousItem) {
+    navItems.forEach(item => {
+        if(item.textContent.trim() === previousItem.trim()) {
+            item.classList.add('active');
+        }
+    })
+}
+
 navItems.forEach(item => {
     item.addEventListener('click' ,function() {
         navItems.forEach(el => el.classList.remove('active'));
         this.classList.add('active');
+        localStorage.setItem('activeItem' , this.textContent.trim());
     })
 });
+  
