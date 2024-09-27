7563438be3b29b3458a49f5dd2ce95a95dfbfa84
kaya70875 <shortsforturkey@gmail.com>
2024-09-27 13:51:35 +0300

    create setActiveStates function to prevent repeated code

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,19 +1,24 @@
-const navItems = document.querySelectorAll('.list-item');
+function setActiveStates(navItemClass) {
+    const navItems = document.querySelectorAll(navItemClass);
 
-const previousItem = localStorage.getItem('activeItem');
-if (previousItem) {
+    const previousItem = localStorage.getItem('activeItem');
+    if(previousItem) {
+        navItems.forEach(item => {
+            if(item.textContent.trim() === previousItem.trim()) {
+                item.classList.add('active');
+            }
+        })
+    }
+
     navItems.forEach(item => {
-        if(item.textContent.trim() === previousItem.trim()) {
-            item.classList.add('active');
-        }
-    })
+        item.addEventListener('click' ,function() {
+            navItems.forEach(el => el.classList.remove('active'));
+            this.classList.add('active');
+            localStorage.setItem('activeItem' , this.textContent.trim());
+        })
+    });
 }
 
-navItems.forEach(item => {
-    item.addEventListener('click' ,function() {
-        navItems.forEach(el => el.classList.remove('active'));
-        this.classList.add('active');
-        localStorage.setItem('activeItem' , this.textContent.trim());
-    })
-});
+setActiveStates('.list-item');
+setActiveStates('.ellipse-button');
   
